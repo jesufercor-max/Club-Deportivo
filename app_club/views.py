1d53535af90a720d41343d4adf1fbfca65c495e0
Jesus Fernandez <jesufercor@gmail.com>
2025-10-21 11:16:11 +0200

    URL 1

--- app_club/views.py
+++ app_club/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Usuario
 
 # Create your views here.
+
+# Vista 1: Usuario del club
+def usuario(request):
+    usuarios = Usuario.objects.all()
+    return render(request, 'app_club/usuario/usuario.html', {})
